c7c61cc1d5cab4602ae9d68c419b9d10f09e47f2
Jonathan Samudio <jonathan2457@gmail.com>
2018-11-01 14:42:17 -0400

    Added a highlight to the item view.

--- app/src/main/java/com/example/prolificinteractive/digitallibrary/bookDetail/BookDetailActivity.kt
+++ app/src/main/java/com/example/prolificinteractive/digitallibrary/bookDetail/BookDetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.prolificinteractive.digitallibrary.bookDetail
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.prolificinteractive.digitallibrary.R
 
 class BookDetailActivity : AppCompatActivity() {
@@ -9,5 +10,17 @@ class BookDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        onBackPressed()
+        return true
     }
 }
