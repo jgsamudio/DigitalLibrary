767798972ef4621ff512d32edfe5d4e1a09da1e8
Jonathan Samudio <jonathan2457@gmail.com>
2018-11-06 09:53:21 -0500

    Added bottom sheet

--- app/src/main/java/com/example/prolificinteractive/digitallibrary/bookDetail/BookDetailActivity.kt
+++ app/src/main/java/com/example/prolificinteractive/digitallibrary/bookDetail/BookDetailActivity.kt
@@ -15,7 +15,7 @@ class BookDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_detail)
+        setContentView(R.layout.container)
         setupActionBar()
 
         val book = intent.getSerializableExtra(SELECTED_BOOK) as Book
